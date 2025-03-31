548aa14bcfd549d36c62abb076b66281a27d91ff
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-03-31 11:50:10 +0200

    Configurado proxy y probada conexion con spring boot

--- src/app/demo/DemoServicio.ts
+++ src/app/demo/DemoServicio.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class DemoServicio {
-  private apiUrl = 'http://localhost:8080/api/hola-mundo';
+  private apiUrl:string = `${environment.apiUrl}/hola-mundo`;
   constructor(private http: HttpClient) { }
 
   getMensaje(): Observable<string> {
