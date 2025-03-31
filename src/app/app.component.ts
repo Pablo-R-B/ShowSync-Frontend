08b4821c5af4199e8cbe81bfda7205f639d8f1a5
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-03-31 17:47:35 +0200

    Añadido router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import {DemoServicio} from './demo/DemoServicio';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
