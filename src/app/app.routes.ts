908261a1352f501d844fd8035a0dafa602500829
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-03-31 17:46:49 +0200

    Añadida ruta catalogo-artistas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {CatalogoArtistasComponent} from '../componentes/catalogo-artistas/catalogo-artistas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'catalogo-artistas', component:CatalogoArtistasComponent, pathMatch:'full'},
+];
