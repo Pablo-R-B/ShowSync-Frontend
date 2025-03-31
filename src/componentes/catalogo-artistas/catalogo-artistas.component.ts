7f3490a6bab74a5638e1abb7cf9c0046af802dd7
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-03-31 17:49:27 +0200

    Creado componente catalogo-artistas

---
+++ src/componentes/catalogo-artistas/catalogo-artistas.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {Artistas} from '../../interfaces/artistas';
+import {ArtistasService} from '../../servicios/artistas.service';
+import {NgForOf, NgIf} from '@angular/common';
+
+
+
+@Component({
+  selector: 'app-catalogo-artistas',
+  imports: [
+    NgIf,
+    NgForOf
+  ],
+  templateUrl: './catalogo-artistas.component.html',
+  styleUrl: './catalogo-artistas.component.css'
+})
+export class CatalogoArtistasComponent {
+
+  artistasLista: Artistas[] = [];
+  errorMsj:string ='';
+
+  constructor(private artistasService: ArtistasService) {
+  }
+
+  ngOnInit() {
+    this.listarArtistas();
+
+  }
+
+  listarArtistas():void {
+    this.artistasService.listarArtistasConGeneros().subscribe({
+      next: results => {
+        this.artistasLista = results;
+      }, error: error => {
+        this.errorMsj = error.message;
+      }
+    })
+  }
+}
