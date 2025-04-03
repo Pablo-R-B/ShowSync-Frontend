5189689943aad20d08676852be45d6dd9d8fd8fe
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-04-03 19:45:22 +0200

    Barra navegacion en construccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {DemoServicio} from './demo/DemoServicio';
 import {RouterOutlet} from '@angular/router';
+import {BarraNavegacionComponent} from '../componentes/barra-navegacion/barra-navegacion.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    BarraNavegacionComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
