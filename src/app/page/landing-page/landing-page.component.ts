033495f987f24074cc031de57073cdcb0b438fc9
Lisseth <lbenturayuca@safareyes.es>
2025-04-08 23:27:37 +0200

    creacion de la landing page

---
+++ src/app/page/landing-page/landing-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+
+}
