19076932fccbd9f24619d25a85834d744b595971
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-05-11 13:39:10 +0200

    Creado metodo enviar solicitud promotor a artista

--- src/app/pages/perfil-artista-prueba/perfil-artista-prueba.component.ts
+++ src/app/pages/perfil-artista-prueba/perfil-artista-prueba.component.ts
@@ -1,21 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Artistas} from '../../interfaces/artistas';
 import {ArtistasService} from '../../servicios/artistas.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {NgIf} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {EventoDTO, PromotoresService} from '../../servicios/PromotoresService';
+import {AuthService} from '../../servicios/auth.service';
+import {PostulacionEventoService} from '../../servicios/postulacion-evento.service';
+
 
 @Component({
   selector: 'app-perfil-artista-prueba',
   imports: [
-    NgIf
+    NgIf,
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './perfil-artista-prueba.component.html',
+  standalone: true,
   styleUrl: './perfil-artista-prueba.component.css'
 })
 export class PerfilArtistaPruebaComponent implements OnInit{
   artista:Artistas | undefined;
+  @Input() mostrarModal = false;
+  eventoSeleccionado!:number
+  IdUsuarioDePromotor!: number;
+  eventos: EventoDTO[] = [];
+  artistaId!: number;
 
-  constructor(private artistasService:ArtistasService, private route: ActivatedRoute, private router: Router) {
+  constructor(private artistasService:ArtistasService, private route: ActivatedRoute,
+              private promotoresService: PromotoresService, private authService: AuthService,
+              private postulacionService:PostulacionEventoService) {
   }
 
   ngOnInit() {
@@ -30,8 +45,59 @@ export class PerfilArtistaPruebaComponent implements OnInit{
         console.error('ID no encontrado en la URL');
       }
     });
+    this.IdUsuarioDePromotor = this.authService.userId;
+    console.log("Usuario promtor", this.IdUsuarioDePromotor)
+    this.cargarEventosPromotor();
+    this.artistaId = Number(this.route.snapshot.paramMap.get('id'));
+
+
   }
 
 
 
-}
+  cargarEventosPromotor(): void {
+    this.promotoresService.listarEventosPorUsuarioDePromotor(this.IdUsuarioDePromotor).subscribe({
+      next:(data: EventoDTO[]) => this.eventos = data,
+      error: err => console.error('Error al cargar eventos:', err)
+      }
+
+    )
+
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
+  }
+
+  alerta = {
+    tipo: '' as'enviada' | 'noenviada',
+    mensaje:'',
+    visible:false
+  }
+
+  private alertaSolicitud(tipo:'enviada' | 'noenviada', mensaje:string):  void{
+    this.alerta = {tipo, mensaje, visible:true}
+    setTimeout(()=>{
+      this.alerta.visible = false;
+    }, 5000);
+  }
+
+  enviarOferta() {
+    this.postulacionService.enviarOfertaEventoArtista(this.eventoSeleccionado, this.artistaId)
+      .subscribe({
+        next: response =>{
+          if(response.status === 201){
+            this.alertaSolicitud('enviada', 'Solicitud enviada')
+          }else{
+            this.alertaSolicitud('noenviada', 'Respuesta inesperada. No se puedo enviar la solicitud')
+            console.error(`Respuesta inesperada: ${response.status}`)
+          }
+        },
+        error: err =>{
+          console.error('Error en la oferta', err)
+          this.alertaSolicitud('noenviada', 'No se pudo enviar la solicicitud')
+        }
+
+      })
+  }
+  }
