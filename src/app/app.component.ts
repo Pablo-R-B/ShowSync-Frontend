3c1101846969f8d668900ae3dc6de65fe627050a
alba <sfg@sfg.es>
2025-05-24 21:09:58 +0200

    Solucionado espaciado de las paginas que no tienen header.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {filter} from 'rxjs';
 import {ButtonModule} from 'primeng/button';
 import { NgIf } from '@angular/common';
-import {FullCalendarModule} from '@fullcalendar/angular';
 import {HeaderComponent} from './componentes/header/header.component';
 import {FooterComponent} from './componentes/footer/footer.component';
 import {SharedModule} from 'primeng/api';
