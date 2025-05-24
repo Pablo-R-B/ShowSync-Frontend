efc4dcd46005d8e4407a71ddee143f9c5794a88a
alba <sfg@sfg.es>
2025-05-25 01:00:45 +0200

    Mejora para qu eel horario sea en español y el resto del formulario sean todos requisitos para poder enviar el evento.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {filter} from 'rxjs';
 import {ButtonModule} from 'primeng/button';
-import { NgIf } from '@angular/common';
+import {NgIf, registerLocaleData} from '@angular/common';
 import {HeaderComponent} from './componentes/header/header.component';
 import {FooterComponent} from './componentes/footer/footer.component';
 import {SharedModule} from 'primeng/api';
-
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs, 'es');
 
 // @ts-ignore
 @Component({
